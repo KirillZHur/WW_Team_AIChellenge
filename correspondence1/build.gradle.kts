c66243105e6e64aac3e6cf8475af7ca039ddcefc
Dazz1ee <ikatin.2003.sokol@gmail.com>
2025-11-29 07:00:24 +0300

    feat(backend-2): add llm integration

--- correspondence1/build.gradle.kts
+++ correspondence1/build.gradle.kts
@@ -3,7 +3,7 @@ import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.13"
+    id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
     id("org.openapi.generator") version "7.14.0"
